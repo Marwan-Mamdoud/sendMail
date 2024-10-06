c6d6f6d0122e0fc870745a07262271f14c022f97
Marwan <marwanmamdouh169@gmail.com>
2024-10-06 19:31:56 +0300

    mail

---
+++ index.js
@@ -0,0 +1,51 @@
+import express from "express";
+import cors from "cors";
+import nodemailer from "nodemailer";
+import cookieParser from "cookie-parser";
+const app = express();
+app.listen(5000, () => {
+  console.log("done");
+});
+
+app.use(
+  cors({
+    origin: "*", // النطاق المسموح به
+    credentials: true, // السماح بإرسال معلومات الاعتماد
+  })
+);
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
+
+app.post("/api/sendMail", async (req, res, next) => {
+  try {
+    const { name, email, phone, rooms, state, message } = req.body;
+    console.log({ name, email, phone, rooms, state, message });
+    const transport = nodemailer.createTransport({
+      service: "gmail",
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
+      auth: {
+        user: "marwanmamdouh159@gmail.com",
+        pass: "casa yloo prts fdds",
+      },
+    });
+    const mailOption = {
+      from: {
+        name: "marwan",
+        address: '"marwanmamdouh159@gmail.com"',
+      },
+      to: "marwanmamdouh159@gmail.com",
+      subject: "New Booking",
+      text: "Done",
+      html: `<b>name: ${name}</b><br/><b>email: ${email}</b><br/> <b>phone: ${phone}</b><br/><b>Number of Rooms: ${rooms}</b><br/><b>Name of State: ${state}</b><br/><b>Message: ${message}</b><br/>`,
+    };
+    await transport.sendMail(mailOption);
+    console.log("done");
+    return res.status(200).json({ message: "Done Send mail" });
+  } catch (error) {
+    console.log(error.message);
+  }
+});
