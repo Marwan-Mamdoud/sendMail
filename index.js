3a8a46298726d7e2d7c4ed4c300d49e628a1bdd4
Marwan <marwanmamdouh169@gmail.com>
2024-10-08 15:13:14 +0300

    mail

--- index.js
+++ index.js
@@ -20,7 +20,8 @@ app.use(cookieParser());
 
 app.post("/api/sendMail", async (req, res, next) => {
   try {
-    const { name, email, phone, rooms, state, message } = req.body;
+    const { name, phone, startDate, endDate, occasion, rooms, people } =
+      req.body;
     console.log({ name, email, phone, rooms, state, message });
     const transport = nodemailer.createTransport({
       service: "gmail",
@@ -41,7 +42,7 @@ app.post("/api/sendMail", async (req, res, next) => {
       to: "marwanmamdouh159@gmail.com",
       subject: "real estate website",
       text: "You just got a form submission!",
-      html: `<br/><p>You just got a form submission!</p><br/><br/><b>Form</b><br/><p>Booking From</p><br/><br/><b>Site</b><br/><p>Real State Site</p><br/><br/><b>Submitted content</b> <br/><b>name: ${name}</b><br/><b>email: ${email}</b><br/> <b>phone: ${phone}</b><br/><b>Number of Rooms: ${rooms}</b><br/><b>Name of State: ${state}</b><br/><b>Message: ${message}</b><br/>`,
+      html: `<br/><p>You just got a form submission!</p><br/><br/><b>Form</b><br/><p>Booking From</p><br/><br/><b>Site</b><br/><p>Real State Site</p><br/><br/><b>Submitted content</b> <br/><b>name: ${name}</b><br/><b>phone: ${phone}</b><br/><b>Occasion: ${occasion}</b><br/><b>Number of Rooms: ${rooms}</b><br/><b>Number of People: ${people}</b><br/><b>Start Date: ${startDate}</b><br/> <b>End Date: ${endDate}</b><br/>`,
     };
     await transport.sendMail(mailOption);
     console.log("done");
